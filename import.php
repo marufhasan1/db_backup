da058631b952f5431319c2ce9a8be6d480c92c75
Maruf Hasan <emarufhasan@gmail.com>
2017-12-02 16:52:02 +0600

    Include upload and import example
    Include upload and import example

--- import.php
+++ import.php
@@ -1,7 +1,7 @@
 	<?php
 		include('db_backup_library.php');
 		$dbbackup = new db_backup;
-		$dbbackup->connect("localhost","root","","myci1");
+		$dbbackup->connect("localhost","root","","test");
 		$dbbackup->backup();
 		if($dbbackup->db_import("test.sql")){
 			echo "Database Successfully Imported";
